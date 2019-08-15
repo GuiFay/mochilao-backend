4510d19192b5e45bed87f26018607c1d0dd78bc9
Guilherme Fay Vergara <guilherme.vergara@mp.intra>
2019-08-15 12:07:11 -0300

    getting prices from api

--- src/controllers/priceController.js
+++ src/controllers/priceController.js
@@ -3,79 +3,66 @@ const axios = require('axios');
 var solver = require('node-tspsolver')
 var request = require("request");
 
-async function costMatrix(country, currency, locale, originplace, destinationplace, outboundpartialdate) {
-    try {
-        const config = {
-            headers: {
-                'x-rapidapi-host': 'skyscanner-skyscanner-flight-search-v1.p.rapidapi.com',
-                'x-rapidapi-key': '27dd665eafmsh05e7e181465dbd8p100153jsnb5d709e7fcc2'
-            }
-        };
-
-        const response = await axios.get(` https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browsequotes/v1.0/${country}/${currency}/${locale}/${originplace}/${destinationplace}/${outboundpartialdate}`, config)
-        return response.data
-    }
-    catch (err) {
-        console.log('fetch failed', err);
-    }
-
-    // const price = response_data.Quotes[0].MinPrice
-    // console.log(response_data)
-};
-
 module.exports = {
-    index(req, res) {
+    async index(req, res) {
+        try {
+            const country = "BR";
+            const currency = "BRL";
+            const locale = "pt-BR";
+            const outboundpartialdate = "2019-08-16";
+            const { cities } = req.body
+            const cities_combination = [].concat(...cities.map(
+                (v, i) => cities.slice(i + 1).map(w => [v, w]))
+            );
+            let prices = [];
+
+            const config = {
+                headers: {
+                    'x-rapidapi-host': 'skyscanner-skyscanner-flight-search-v1.p.rapidapi.com',
+                    'x-rapidapi-key': '27dd665eafmsh05e7e181465dbd8p100153jsnb5d709e7fcc2'
+                }
+            };
+
+            for (var i = 0; i < cities_combination.length; i++) {
+                const response = await axios.get(` https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browsequotes/v1.0/${country}/${currency}/${locale}/${cities_combination[i][0]}/${cities_combination[i][1]}/${outboundpartialdate}`, config)
 
 
-        const prices = []
-        const { cities } = req.body
-        const cities_combination = [].concat(...cities.map(
-            (v, i) => cities.slice(i + 1).map(w => [v, w]))
-        );
-
-        cities_combination.forEach(element => {
-            // console.log(`tentando ${element[0]} -> ${element[1]}`)
-            costMatrix("BR", "BRL", "pt-BR", element[0], element[1], "2019-08-16")
-                .then((data) => {
-                    prices.push(data.Quotes[0].MinPrice)
-                })
-
-            // prices.push("data.Quotes[0].MinPrice")
-        });
+                console.log(`${cities_combination[i][0]} -> ${cities_combination[i][1]} : ${response.data.Quotes[0].MinPrice}`)
+                prices.push(response.data.Quotes[0].MinPrice)
+            };
 
 
-        console.log(prices)
+            let matrix = []
+            for (i in prices) {
+                const line_matrix = []
+                for (z in prices) {
+                    if (prices[i] == prices[z]) {
+                        line_matrix.push(0);
+                    } else {
+                        line_matrix.push(prices[z]);
+                    }
+
+                }
+                matrix.push(line_matrix);
+            }
+            // console.log(matrix)
+
+            solver
+                .solveTsp(matrix, true, {})
+                .then((result) => {
+                    // result.map((city) => {
+                    //     route.push(cities[city]);
+                    // })
+                    return res.json(result) // result is an array of indices specifying the route.
+                });
 
 
-        // const getData = async () => {
-        //     return await Promise.all(cities.map(city => costMatrix("US", "USD", "en-US", city, "JFK-sky", "2019-08-15").Quotes[0].MinPrice
-        //     ))
-        // }   
+            // return res.json({ "ok:": true });
+        } catch (error) {
+            return res.json({ "ok:": false });
+        }
 
-        // const data = getData()
-        // console.log(data)
 
-        // const costMatrix = [
-        //     [0, 1, 3, 4],
-        //     [1, 0, 2, 3],
-        //     [3, 2, 0, 5],
-        //     [4, 3, 5, 0]
-        // ]
-
-        // var route = []
-
-        // solver
-        //     .solveTsp(costMatrix, true, {})
-        //     .then((result) => {
-        //         result.map((city) => {
-        //             route.push(cities[city]);
-        //         })
-        //         return res.json(route) // result is an array of indices specifying the route.
-        //     });
-
-        // a = await costMatrix()      
-
-        return res.json({ "ok:": true });
 
     },
 }
